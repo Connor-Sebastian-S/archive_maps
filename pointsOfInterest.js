b7a674b59bd3b1d00c775379a7fa78e064f7a383
ConnorS <35559977+Connor-Sebastian-S@users.noreply.github.com>
2024-06-06 21:16:41 +0100

    Update pointsOfInterest.js

--- pointsOfInterest.js
+++ pointsOfInterest.js
@@ -1,31 +1,7 @@
 function parseCSV(csvText) {
-    const lines = csvText.split('\n');
-    const headers = lines[0].split(',');
-    const pointsData = [];
 
-    for (let i = 1; i < lines.length; i++) {
-        if (!lines[i]) continue; // skip empty lines
-        const currentLine = lines[i].split(',');
-
-        const point = {};
-        for (let j = 0; j < headers.length; j++) {
-            const key = headers[j].trim();
-            let value = currentLine[j].trim();
-
-            // Convert latitude and longitude to numbers
-            if (key === 'latitude' || key === 'longitude') {
-                value = parseFloat(value);
-            }
-
-            // Convert startYear and endYear to numbers
-            if (key === 'startYear' || key === 'endYear') {
-                value = parseInt(value);
-            }
-
-            point[key] = value;
-        }
-        pointsData.push(point);
-    }
+    const options = { header: true };
+    const pointsData = Papa.parse(csvText, options);
 
     return pointsData;
 }
