42012b0f0950852318b7d508f2db36e0d8ed4122
ConnorS <35559977+Connor-Sebastian-S@users.noreply.github.com>
2024-06-06 17:14:34 +0100

    Update pointsOfInterest.js

--- pointsOfInterest.js
+++ pointsOfInterest.js
@@ -73,4 +73,4 @@ const fs = require('fs');
 // Read the Excel file
 const workbook = XLSX.readFile('./data/points_data.xlsx');
 const sheet_name_list = workbook.SheetNames;
-const xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+var pointsData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
