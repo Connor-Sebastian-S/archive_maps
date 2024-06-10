ad22f0f05c36a08dd6a9365d94c5d75c89cdb3c3
ConnorS <35559977+Connor-Sebastian-S@users.noreply.github.com>
2024-06-10 12:19:51 +0100

    Fixed map, fixed markers, made photos enlarge when clicked

--- mapDefinition.js
+++ mapDefinition.js
@@ -2,31 +2,37 @@
 var mainOverlayLayers = {
 
     "1747": L.tileLayer('data/original_maps/1747-1752/{z}/{x}/{y}.png', {
-        attribution: "Produced by x in 1747, as part of x",
+        attribution: "Roy Military Survey of Scotland, 1747-55",
         
         maxZoom: 18,
         maxNativeZoom: 16
     }),
-    "1843": L.tileLayer('data/original_maps/1843-1882/{z}/{x}/{y}.png', {
-        attribution: "Produced by x in 1823, as part of x",
+    "1856": L.tileLayer('data/original_maps/1856-1891/{z}/{x}/{y}.png', {
+        attribution: "One-inch, Scotland, 1st Edition, 1856-1891, Publication date: 1867, produced by Ordnance Survey",
+        
+        maxZoom: 18,
+        maxNativeZoom: 16
+    }),
+    "1864": L.tileLayer('data/original_maps/1830-1880/{z}/{x}/{y}.png', {
+        attribution: "Perthshire LXII, Surveyed: 1864, Published: 1867, Publication date: 1867, produced by Ordnance Survey",
         
         maxZoom: 18,
         maxNativeZoom: 16
     }),
     "1888": L.tileLayer('data/original_maps/1888-1913/{z}/{x}/{y}.png', {
-        attribution: "Perthshire, Sheet LXII, Survey date: 1864, Publication date: 1867, produced by Ordnance Survey",
+        attribution: "Perth and Clackmannan LXII.NW, Surveyed: 1899, Published: 1901, produced by Ordnance Survey",
         
         maxZoom: 18,
         maxNativeZoom: 16
     }),
     "1926": L.tileLayer('data/original_maps/1926-1935/{z}/{x}/{y}.png', {
-        attribution: "Perthshire, Sheet LXII, Survey date: 1864, Publication date: 1867, produced by Ordnance Survey",
+        attribution: "Bartholomew's Half Inch to the Mile Maps of Scotland, 1926-1935",
         
         maxZoom: 18,
         maxNativeZoom: 16
     }),
     "1937": L.tileLayer('data/original_maps/1937-1961/{z}/{x}/{y}.png', {       
-        attribution: "Great Britain - OS 1:25,000 (Outline), 1945-65, produced by Ordnance Survey",
+        attribution: "1:25,000, Great Britain, 1937-1961, produced by Ordnance Survey",
         
         maxZoom: 18,
         maxNativeZoom: 16
@@ -47,7 +53,13 @@ var alternativeOverlayLayers = {
         maxZoom: 18,
         maxNativeZoom: 16
     }),
-    "1843": L.tileLayer('data/redrawn_maps/BASIC/{z}/{x}/{y}.png', {
+    "1856": L.tileLayer('data/redrawn_maps/BASIC/{z}/{x}/{y}.png', {
+        attribution: "Alternative 1856 Map",
+        
+        maxZoom: 18,
+        maxNativeZoom: 16
+    }),
+    "1864": L.tileLayer('data/redrawn_maps/BASIC/{z}/{x}/{y}.png', {
         attribution: "Alternative 1843 Map",
         
         maxZoom: 18,
