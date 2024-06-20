880fa5497dfa7e0aa49a39d7bdd090bd0eab154a
ConnorS <35559977+Connor-Sebastian-S@users.noreply.github.com>
2024-06-20 10:46:02 +0100

    Fixes, nought else really

--- mapDefinition.js
+++ mapDefinition.js
@@ -4,19 +4,25 @@ var mainOverlayLayers = {
         attribution: "In the beginning, there was nought but rivers and forests",
         
         maxZoom: 18,
-        maxNativeZoom: 16
+        maxNativeZoom: 18
     }),
     "1747": L.tileLayer('data/original_maps/1747-1752/{z}/{x}/{y}.png', {
         attribution: "Roy Military Survey of Scotland, 1747-55",
         
         maxZoom: 18,
-        maxNativeZoom: 16
+        maxNativeZoom: 18
+    }),
+    "1780": L.tileLayer('data/original_maps/1780-1847/{z}/{x}/{y}.png', {
+        attribution: "John Wood, 1780-1847, Plan of Dunkeld and Locale",
+        
+        maxZoom: 18,
+        maxNativeZoom: 18
     }),
     "1856": L.tileLayer('data/original_maps/1856-1891/{z}/{x}/{y}.png', {
         attribution: "One-inch, Scotland, 1st Edition, 1856-1891, Publication date: 1867, produced by Ordnance Survey",
         
         maxZoom: 18,
-        maxNativeZoom: 16
+        maxNativeZoom: 15
     }),
     "1864": L.tileLayer('data/original_maps/1830-1880/{z}/{x}/{y}.png', {
         attribution: "Perthshire LXII, Surveyed: 1864, Published: 1867, Publication date: 1867, produced by Ordnance Survey",
@@ -34,7 +40,7 @@ var mainOverlayLayers = {
         attribution: "Bartholomew's Half Inch to the Mile Maps of Scotland, 1926-1935",
         
         maxZoom: 18,
-        maxNativeZoom: 16
+        maxNativeZoom: 15
     }),
     "1937": L.tileLayer('data/original_maps/1937-1961/{z}/{x}/{y}.png', {       
         attribution: "1:25,000, Great Britain, 1937-1961, produced by Ordnance Survey",
@@ -59,6 +65,12 @@ var alternativeOverlayLayers = {
     }),
     "1747": L.tileLayer('data/redrawn_maps/BASIC/{z}/{x}/{y}.png', {
         attribution: "Alternative 1747 Map",
+        
+        maxZoom: 18,
+        maxNativeZoom: 16
+    }),
+    "1780": L.tileLayer('data/redrawn_maps/BASIC/{z}/{x}/{y}.png', {
+        attribution: "Alternative 1780 Map",
         
         maxZoom: 18,
         maxNativeZoom: 16
