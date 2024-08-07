ec9e3402441a31b17b21d8ccee41c8d0f01757c9
ConnorS <35559977+Connor-Sebastian-S@users.noreply.github.com>
2024-08-07 11:33:47 +0100

    Async

---
+++ maps.js
@@ -0,0 +1,69 @@
+// Layers
+var mainOverlayLayers = {
+    "1500": L.tileLayer('data/redrawn_maps/beginning/{z}/{x}/{y}.png', {
+        attribution: "In the beginning, there was nought but rivers and forests",
+        
+        maxZoom: 18,
+        maxNativeZoom: 18
+    }),
+    "1560": L.tileLayer('data/original_maps/1560-1614/{z}/{x}/{y}.png', {
+        attribution: "Timothy Pont, 'Garry, Tummel, and Upper Tay; Dunkeld to Blairgowrie - Pont 23 (front)' - 1560?-1614?",
+        
+        maxZoom: 18,
+        maxNativeZoom: 18
+    }),
+    "1747": L.tileLayer('data/original_maps/1747-1752/{z}/{x}/{y}.png', {
+        attribution: "Roy Military Survey of Scotland, 1747-55",
+        
+        maxZoom: 18,
+        maxNativeZoom: 18
+    }),
+    "1780": L.tileLayer('data/original_maps/1780-1847/{z}/{x}/{y}.png', {
+        attribution: "John Wood, 1780-1847, Plan of Dunkeld and Locale",
+        
+        maxZoom: 18,
+        maxNativeZoom: 18
+    }),
+    "1856": L.tileLayer('data/original_maps/1856-1891/{z}/{x}/{y}.png', {
+        attribution: "One-inch, Scotland, 1st Edition, 1856-1891, Publication date: 1867, produced by Ordnance Survey",
+        
+        maxZoom: 18,
+        maxNativeZoom: 15
+    }),
+    "1864": L.tileLayer('data/original_maps/1830-1880/{z}/{x}/{y}.png', {
+        attribution: "Perthshire LXII, Surveyed: 1864, Published: 1867, Publication date: 1867, produced by Ordnance Survey",
+        
+        maxZoom: 18,
+        maxNativeZoom: 16
+    }),
+    "1888": L.tileLayer('data/original_maps/1888-1913/{z}/{x}/{y}.png', {
+        attribution: "Perth and Clackmannan LXII.NW, Surveyed: 1899, Published: 1901, produced by Ordnance Survey",
+        
+        maxZoom: 18,
+        maxNativeZoom: 16
+    }),
+    "1926": L.tileLayer('data/original_maps/1926-1935/{z}/{x}/{y}.png', {
+        attribution: "Bartholomew's Half Inch to the Mile Maps of Scotland, 1926-1935",
+        
+        maxZoom: 18,
+        maxNativeZoom: 15
+    }),
+    "1937": L.tileLayer('data/original_maps/1937-1961/{z}/{x}/{y}.png', {       
+        attribution: "1:25,000, Great Britain, 1937-1961, produced by Ordnance Survey",
+        
+        maxZoom: 18,
+        maxNativeZoom: 16
+    }),
+    "1961": L.tileLayer('data/original_maps/1961-2024/{z}/{x}/{y}.png', {
+        attribution: "NO04, Revised: 1938 - 1955, Published: 1957, produced by Ordnance Survey",
+        
+        maxZoom: 18,
+        maxNativeZoom: 16
+    })
+};
+
+var ohmLayer = new L.MapboxGL({
+            attribution: "OpenHistoricalMap",
+            style: OHM_MAP_STYLE,
+            accessToken: "no-token"
+        });
---
+++ variables.js
@@ -0,0 +1,28 @@
+// By default, what is the center of the map view
+var mapCenter = [56.563729541410666, -3.584791427303605];
+
+// What is the earliest date in the timeline
+var timelineStartDate = '1200';
+
+// When the page opens, what year does it start on
+var timelineInitDate = '1860';
+
+// The year that the timeline ends on
+var timelineEndYear = '2024';
+
+// The maximum boundaries for the map
+var southWestBounds = L.latLng(56.550324, -3.6201320);
+var northEastBounds = L.latLng(56.580230488, -3.571052);
+
+// After how many seconds does the timeline move automatically
+var autoPlayTimer = 120;
+
+// The text that appears on the toggle map view button
+var switchMessage01 = "Switch to Redrawn Map";
+var switchMessage02 = "Switch to Original Map";
+
+// The text that appears next to the QR code
+var qrCodeMessage = "Scan me with your phone to visit this location!";
+
+// How many seconds does the timeline automove feature take per year
+var timelineInterval = 0.8;
